a4f1517add0c1eb7029584794a709517134dcbee
Eli Wang <sirxenofex@gmail.com>
2014-04-23 01:25:11 +0800

    Preference changes are now sent to the dropdown menu correctly

--- data/dropdown_content.js
+++ data/dropdown_content.js
@@ -2,4 +2,8 @@ $(function() {
     $('input[name="mode"]').val([self.options.prefs.proxyMode]).click(function() {
         self.port.emit("prefsChanged", {name: "proxyMode", value: this.value});
     });
+
+    self.port.on('prefsChanged', function (prefs) {
+        $('input[name="mode"]').val([prefs.proxyMode]);
+    });
 });
