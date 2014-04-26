bf1fb5fe30be80004ba16cc5b700f5dcc2842a14
Eli Wang <sirxenofex@gmail.com>
2014-04-26 15:16:59 +0800

    Fix that the proxy mode was saved but not properly loaded. Subscription is now cached.

--- data/dropdown_content.js
+++ data/dropdown_content.js
@@ -1,18 +1,5 @@
 $(function() {
     var $items = $('#mode-select .item');
-
-    if (typeof addon !== 'undefined') {
-        $items.find("#proxy-" + addon.options.prefs.proxyMode).addClass('active');
-        $items.click(function() {
-            addon.port.emit("prefsChanged", {name: "proxyMode", value: this.id.substring(6)});
-        });
-
-        addon.port.on('prefsChanged', function (prefs) {
-            $items.find("#proxy-" + prefs.proxyMode).addClass('active');
-        });
-    }
-
-
     $items.click(function() {
       $(this)
       .addClass('active')
@@ -20,5 +7,17 @@ $(function() {
       .find('.item')
       .not($(this))
       .removeClass('active');
+
+      addon.port.emit("prefsChanged", {name: "proxyMode", value: this.id.substring(6)});
     });
+
+    if (typeof addon === 'undefined') {
+        addon = { options: { prefs: { proxyMode: 'auto'}}};
+    }
+
+    $items.filter("#proxy-" + addon.options.prefs.proxyMode).click();
+    addon.port.on('prefsChanged', function (prefs) {
+        $items.find("#proxy-" + prefs.proxyMode).click();
+    });
+
 });
